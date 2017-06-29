9ff0544bd90b2b08aac901de5e3da44d99c2be1e
sochetanie <allagorik2901@gmail.com>
2017-06-29 14:48:45 -0400

    done

--- js/events.js
+++ js/events.js
@@ -3,5 +3,30 @@
 $(document).ready(function(){
 
 // call functions here
+getIt()
+frameIt()
+pressIt()
+submitIt()
 
 });
+
+
+function getIt() {
+    $('p').on('click', () => {alert("Hey!")})
+}
+
+function frameIt() {
+    $('img').on('load', () => {
+        $('img').addClass('tasty').css('border', 'solid 2px red')
+    })
+}
+
+function pressIt() {
+    $(document).on('keydown', function(x) {
+        if (x.which === 71){alert('G was pressed')}
+    })
+}
+
+function submitIt() {
+    $('form').on('submit', () => {alert('Your form is going to be submitted now.')})
+}
